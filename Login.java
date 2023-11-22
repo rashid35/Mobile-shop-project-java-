650f966df0a477bc1286dc9d11122b18fe2e0229
ZIAUL ELHAM <106006369+rashid35@users.noreply.github.com>
2023-11-22 19:27:36 +0600

    Login.java

---
+++ Login.java
@@ -0,0 +1,18 @@
+public class Login_Info {
+    private String admin_username="admin";
+    private String admin_password="123";
+    private String employee_username="user";
+    private String employee_password="777";
+    String getAdmin_username(){
+        return admin_username;
+    }
+    String getAdmin_password(){
+        return admin_password;
+    }
+    String getEmployee_username(){
+        return employee_username;
+    }
+    String getEmployee_password(){
+        return employee_password;
+    }
+}
